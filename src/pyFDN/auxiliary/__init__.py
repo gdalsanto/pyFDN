bdca5b76c84485232704a77e854601862622368e
Sebastian Jiro Schlecht <45119263+SebastianJiroSchlecht@users.noreply.github.com>
2026-02-20 18:50:49 +0100

    sdn example

--- src/pyFDN/auxiliary/__init__.py
+++ src/pyFDN/auxiliary/__init__.py
@@ -1,4 +1,12 @@
-"""Auxiliary modules (utils, acoustics, etc.)."""
+"""Auxiliary modules (utils, acoustics, SDN, flamo wrappers, etc.)."""
 from .utils import skew
+from .SDN import SDN
+from .flamo import gain_module, delay_module, sos_filter_module
 
-__all__ = ["skew"]
+__all__ = [
+    "skew",
+    "SDN",
+    "gain_module",
+    "delay_module",
+    "sos_filter_module",
+]
